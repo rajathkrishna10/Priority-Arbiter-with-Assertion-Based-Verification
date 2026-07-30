113be165f02636884310076bddaa2d717257532e
ChethanKumarr2276 <184110760+ChethanKumarr2276@users.noreply.github.com>
2026-07-30 12:46:32 +0530

    Add Order of Execution documentation

---
+++ Order_of_execution.m
@@ -0,0 +1,6 @@
+interface.sv, transaction.sv
+  → arbiter_assertions.sv, design.sv
+    → generator.sv, driver.sv, monitor.sv, scoreboard.sv, coverage.sv
+      → environment.sv
+        → test.sv
+          → testbench.sv
